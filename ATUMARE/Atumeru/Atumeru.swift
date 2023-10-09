c632fafbc5cbe1cffc1687a4791ea34025811e0b
wai-1002 <wai.create2021@gmail.com>
2023-10-09 18:13:51 +0900

    init

---
+++ ATUMARE/Atumeru/Atumeru.swift
@@ -0,0 +1,8 @@
+//
+//  Atumeru.swift
+//  ATUMARE
+//
+//  Created by 若生優希 on 2023/10/09.
+//
+
+import Foundation
---
+++ ATUMARE/Atumeru/AtumeruMap.swift
@@ -0,0 +1,8 @@
+//
+//  AtumeruMap.swift
+//  ATUMARE
+//
+//  Created by 若生優希 on 2023/10/09.
+//
+
+import Foundation
