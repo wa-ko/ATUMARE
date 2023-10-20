6437f448b1f264c6db23a1dbb36963164b2db643
Taiki <t9034869@icloud.com>
2023-10-20 21:34:31 +0900

    見た目の部分を途中まで作成しました。

--- ATUMARE/Atumeru/Atumeru.swift
+++ ATUMARE/Atumeru/Atumeru.swift
@@ -5,4 +5,53 @@
 //  Created by 若生優希 on 2023/10/09.
 //
 
+import SwiftUI
 import Foundation
+
+struct Atumeru: View {
+    var body: some View {
+        let red: Double = 197 / 255.0
+        let green: Double = 198 / 255.0
+        let blue: Double = 182 / 255.0
+        let customColor = Color(red: red, green: green, blue: blue)
+        @State var name = ""
+        @State var isShowingView: Bool = false
+
+        return ZStack {
+            Color(customColor)
+                .ignoresSafeArea()
+            
+            Rectangle()
+                .foregroundColor(Color(red: 0.85, green: 0.85, blue: 0.85))
+                .frame(width: 352, height: 590)
+                .cornerRadius(30)
+            
+            VStack {
+                VStack(spacing: 0) {
+                    Text("名前")
+                        .padding(.bottom, 0)
+                    TextField("あなたの名前", text: $name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 200)
+                        .padding()
+                }
+                VStack(spacing: 15) {
+                    Text("出発地")
+                        .padding(.bottom, 0)
+                    Button {
+                        isShowingView.toggle()
+                    } label: {
+                        Text("こちらをタップ")
+                    }
+                    .sheet(isPresented: $isShowingView) {
+                        AtumeruWeiting()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    Atumeru()
+}
--- ATUMARE/Atumeru/AtumeruWeiting.swift
+++ ATUMARE/Atumeru/AtumeruWeiting.swift
@@ -5,4 +5,46 @@
 //  Created by 若生優希 on 2023/10/09.
 //
 
+import SwiftUI
 import Foundation
+
+struct AtumeruWeiting: View {
+    var body: some View {
+        let red: Double = 197 / 255.0
+        let green: Double = 198 / 255.0
+        let blue: Double = 182 / 255.0
+
+        let customColor = Color(red: red, green: green, blue: blue)
+
+        return ZStack {
+            Color(customColor)
+                .ignoresSafeArea()
+            
+            Rectangle()
+                .foregroundColor(Color(red: 0.85, green: 0.85, blue: 0.85))
+                .frame(width: 352, height: 590)
+                .cornerRadius(30)
+
+            VStack {
+                Text("待機中...")
+                    .font(.title3)
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0..<100, id: \.self) { _ in
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 298, height: 52)
+                            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+                            .cornerRadius(50)
+                        }
+                    }
+                }
+                .frame(height: 510)
+            }
+        }
+    }
+}
+
+#Preview {
+    AtumeruWeiting()
+}
